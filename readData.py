d733deb7ce8734a92076b8563d6a7111a4ea2bbb
Colby Rome <cdr013@bucknell.edu>
2015-11-09 21:13:07 -0500

    Documented code

--- readData.py
+++ readData.py
@@ -8,24 +8,31 @@ import numpy as np
 from matplotlib import pyplot as plt
 import re
 
-NUM_SECS = 300 # Time axis of graphs
+NUM_SECS = 300 # Size of time axis for graphing
 
 def receiving(ser):
-    ''' This function plots data from the SDI-12 sensor
+    ''' This function plots data from the SDI-12 sensor in realtime.
+        Input: a valid instance of a serial class
+        Output: a real-time plot of the data from an SDI-12 sensor
     '''
+
     new = True # Initialize graphs to initial y-values (not 0)
     plt.ion() # Interactive plot
+    
+    # Create lists for holding sensor data
     temp = [0]*NUM_SECS
     depth = [0]*NUM_SECS
     cond = [0]*NUM_SECS
     plt.figure(figsize=(10,10)) # Adjust the figsize
 
     # Temperature
-    plt.subplot(311)
+    plt.subplot(311) # Divides figure into 3x1 grid
     plt.title('Current Decagon Sensor Readings')
     plt.xlabel('seconds')
     plt.ylabel('Temperature (C)')
     temp_x, = plt.plot(temp)
+    # plot() returns a list of Line2D objects. The comma unpacks
+    # the single value into temp_x.
 
     # Depth
     plt.subplot(312)
@@ -40,21 +47,28 @@ def receiving(ser):
     cond_x, = plt.plot(cond)
 
     while True:
-        # Read from serial line; decode into ascii string
+        # Read from serial line; decode binary into ascii string
         last_received = ser.readline().decode('ascii')
 
-        # Example response from sensor:
+        # Example response from sensor (indicating depth, temp, conductivity);
         # 0+130+22.3+283 OR
-        # 0-11+22.3+0 (This is invalid; can't have negative depth)
+        # 0-11+22.3+0 (This is invalid; cannot have negative depth)
+        # The sensor specifies that physical damage will occur if the 
+        # temperature is less than 0C, so we will not plot values <0.
         
         # regex matching expression:
-        # ^[0][+-][0-9]+[+-][0-9]+.[0-9][+-][0-9]+
+        # ^[0][+][0-9]+[+-][0-9]+.[0-9][+-][0-9]+
 
+        # Check for a valid string received:
         p = re.compile('^[0][+][0-9]+[+-][0-9]+.[0-9][+-][0-9]+')
         match = p.match(last_received)
+
         print('received: ', last_received)
         if match is not None: # If received valid string
             print(last_received.split('+'))
+
+            # Since the string is guaranteed to be valid, we parse using
+            # the .split() function
             newTemp = float(last_received.split('+')[2])
             print(last_received.split('+')[2])
             newDepth = -float(last_received.split('+')[1]) 
@@ -108,5 +122,6 @@ def receiving(ser):
 
 if __name__ == '__main__':
     # Modify the serial port as necessary
+    # TODO: make the serial port a command line argument?
     ser = serial.Serial('/dev/ttyACM0', 9600);
     receiving(ser)
